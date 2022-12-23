ca7e6012cce6f09947b55ef4eadcf2c5f8a5da84
Shobhit Singh <95973429+IITianShobhitsingh@users.noreply.github.com>
2022-12-23 10:36:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//for responsive navbar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+ 
+//for scroll up 
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
